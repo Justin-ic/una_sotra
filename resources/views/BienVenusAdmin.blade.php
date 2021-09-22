f8e093b97a29dcc4386e53e05c15ddf7ec11a178
Justin-ic <gnjustin.ic@gmail.com>
2021-09-22 17:02:22 +0000

    Correction BienvenusAdmin =>  bienvenusAdmin

--- resources/views/BienVenusAdmin.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('contenu')
-<div class="BienvenusAdmin  d-flex justify-content-center align-items-center">
-	<h1>Bienvénus Administrateur JUSTIN</h1>
-</div>
-@endsection  
