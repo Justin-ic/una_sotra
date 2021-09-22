734cc7f147cf8fe436f720276aec885d7b156e21
Justin-ic <gnjustin.ic@gmail.com>
2021-09-22 18:48:07 +0000

    Revision du App => app

--- resources/views/bienVenusAdmin.blade.php
+++ resources/views/bienVenusAdmin.blade.php
@@ -5,3 +5,24 @@
 	<h1>Bienvénus Administrateur </h1>
 </div>
 <!-- @endsection   -->
+
+
+<!-- 
+
+git status
+git add *
+git commit -m ""
+
+git push origin master 
+
+git push heroku master
+
+
+
+
+heroku run php artisan migrate 
+
+heroku pg:reset DATABASE_URL --confirm glacial-everglades-43629
+
+
+ -->
