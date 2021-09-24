3a47c44d5811e629f259540031412c3f2a319b5e
Justin-ic <gnjustin.ic@gmail.com>
2021-09-24 04:17:52 +0000

    Corrections

--- resources/views/inclusions/header.blade.php
+++ resources/views/inclusions/header.blade.php
@@ -25,7 +25,7 @@
               <a class="nav-link" href="{{route('descriptions.index')}}">Descriptions</a> 
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="{{route('bilan_clients.index.index')}}">Bilan clients</a> 
+              <a class="nav-link" href="{{route('bilan_clients.index')}}">Bilan clients</a> 
           </li>
           <li class="nav-item">
               <a class="nav-link" href="clientBienvenue">clientBienvenue</a> 
