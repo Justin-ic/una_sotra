d601ce189c67da4bc3cd76d23fc8d71eafeb8a84
Justin-ic <gnjustin.ic@gmail.com>
2021-11-12 07:01:45 +0000

    Corection URL Temps restatant dans js

--- public/styles/js/clientLocation.js
+++ public/styles/js/clientLocation.js
@@ -220,8 +220,8 @@ function AfficheMarqueurs(){
 
         // url = "{{route('APIMarqueurs')}}"; /*Pas de {{ }} ici car ce n'est pas un blade*/
         url = "http://localhost/una_sotra/public/APIMarqueurs";
-        url = "https://glacial-everglades-43629.herokuapp.com/public/APIMarqueurs";
-        // alert(url);
+        url = "https://glacial-everglades-43629.herokuapp.com/APIMarqueurs";
+        alert(url);
         xhttp.open("GET",url , true);
         xhttp.send();
 
